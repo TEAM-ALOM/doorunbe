b29f7f8c9ca901e1220902ea57de7a0cc1293b6f
ihtblwFIZ <vthyrachel@naver.com>
2024-11-18 15:24:21 +0900

    docs: ItemController에 swagger @Operation 추가
    #5

--- src/main/java/com/alom/dorundorunbe/domain/item/controller/ItemController.java
+++ src/main/java/com/alom/dorundorunbe/domain/item/controller/ItemController.java
@@ -4,6 +4,7 @@ import com.alom.dorundorunbe.domain.item.domain.ItemCategory;
 import com.alom.dorundorunbe.domain.item.dto.EquippedItemResponseDto;
 import com.alom.dorundorunbe.domain.item.dto.ItemResponseDto;
 import com.alom.dorundorunbe.domain.item.service.ItemService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +19,14 @@ public class ItemController {
     private final ItemService itemService;
 
     @GetMapping("/{itemCategory}")
+    @Operation(summary = "아이템 목록 조회", description = "카테고리별로 아이템 목록을 반환합니다(소유한 아이템 우선 정렬)")
     public ResponseEntity<List<ItemResponseDto>> getItemByCategory(@PathVariable("itemCategory") ItemCategory itemCategory,
                                                                    @RequestParam("userId") Long userId) {
         return ResponseEntity.ok(itemService.getItemByCategory(itemCategory, userId));
     }
 
     @PostMapping("/{itemId}")
+    @Operation(summary = "아이템 구매", description = "캐시로 아이템을 구매합니다")
     public ResponseEntity<Void> purchaseItem(@PathVariable("itemId") Long itemId,
                                              @RequestParam("userId") Long userId) {
         itemService.purchaseItem(itemId, userId);
@@ -31,18 +34,21 @@ public class ItemController {
     }
 
     @PostMapping("/{itemId}/equipped")
+    @Operation(summary = "아이템 착용", description = "아이템 착용 후 착용 중인 아이템 목록을 반환합니다")
     public ResponseEntity<List<EquippedItemResponseDto>> equippedItem(@PathVariable("itemId") Long itemId,
                                                                    @RequestParam("userId") Long userId) {
         return ResponseEntity.ok(itemService.equippedItem(itemId, userId));
     }
 
     @PostMapping("/{itemId}/unequipped")
+    @Operation(summary = "아이템 착용 해제", description = "아이템 착용 해제 후 착용 중인 아이템 목록을 반환합니다")
     public ResponseEntity<List<EquippedItemResponseDto>> unequippedItem(@PathVariable("itemId") Long itemId,
                                                                      @RequestParam("userId") Long userId) {
         return ResponseEntity.ok(itemService.unequippedItem(itemId, userId));
     }
 
     @GetMapping("/equipped")
+    @Operation(summary = "착용한 아이템 목록 조회", description = "착용한 아이템 목록을 반환합니다")
     public ResponseEntity<List<EquippedItemResponseDto>> getEquippedItem(@RequestParam("userId") Long userId) {
         return ResponseEntity.ok(itemService.getEquippedItem(userId));
     }
