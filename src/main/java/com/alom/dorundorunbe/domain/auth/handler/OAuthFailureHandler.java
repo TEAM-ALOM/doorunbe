5b55fcdf4a12bedae3cb5de5b7ae1ee7d93d4027
ihtblwFIZ <vthyrachel@naver.com>
2024-11-21 07:02:38 +0900

    feat: oauth 로그인 핸들러 작성

--- src/main/java/com/alom/dorundorunbe/domain/auth/handler/OAuthFailureHandler.java
+++ src/main/java/com/alom/dorundorunbe/domain/auth/handler/OAuthFailureHandler.java
@@ -1,8 +1,9 @@
 package com.alom.dorundorunbe.domain.auth.handler;
 
-import jakarta.servlet.ServletException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -11,8 +12,17 @@ import java.io.IOException;
 
 @Component
 public class OAuthFailureHandler implements AuthenticationFailureHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        System.out.println("로그인에 실패했습니다.");
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.getWriter().write(objectMapper.writeValueAsString("로그인에 실패하였습니다."));
     }
 }
--- src/main/java/com/alom/dorundorunbe/domain/auth/handler/OAuthSuccessHandler.java
+++ src/main/java/com/alom/dorundorunbe/domain/auth/handler/OAuthSuccessHandler.java
@@ -1,18 +1,35 @@
 package com.alom.dorundorunbe.domain.auth.handler;
 
-import jakarta.servlet.ServletException;
+import com.alom.dorundorunbe.domain.auth.dto.PrincipalUserDetails;
+import com.alom.dorundorunbe.domain.auth.provider.JwtTokenProvider;
+import com.alom.dorundorunbe.domain.auth.token.AccessToken;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Component
+@RequiredArgsConstructor
 public class OAuthSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        System.out.println("로그인이 성공했습니다.");
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) {
+        // 유저 정보 추출 -> jwt access 토큰 생성 -> access 토큰을 헤더에 저장
+
+        PrincipalUserDetails userDetails = (PrincipalUserDetails) authentication.getPrincipal();
+
+        AccessToken accessToken = jwtTokenProvider.generateAccessToken(userDetails.getUsername());
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpStatus.OK.value());
+        response.setHeader("Authorization", "Bearer " + accessToken.token());
     }
 }
