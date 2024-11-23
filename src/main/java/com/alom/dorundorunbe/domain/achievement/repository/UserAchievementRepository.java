c9cc799bfff3ccc29fb840d68ead0803b788310a
KyunghoYeom <hoyeom1269@naver.com>
2024-11-23 12:57:54 +0900

    AchievementRepository , UserAchievementRepository 작성

--- src/main/java/com/alom/dorundorunbe/domain/achievement/repository/UserAchievementRepository.java
+++ src/main/java/com/alom/dorundorunbe/domain/achievement/repository/UserAchievementRepository.java
@@ -4,7 +4,10 @@ import com.alom.dorundorunbe.domain.achievement.domain.UserAchievement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserAchievementRepository extends JpaRepository<UserAchievement, Long> {
-    List<UserAchievement> findAllByUserName(String name);
+    List<UserAchievement> findAllByUserName(String name); //my page 에서 사용
+    boolean existsByUserIdAndAchievementId(Long userId, Long achievementId);
+    Optional<UserAchievement> findByUserIdAndAchievementId(Long userId, Long achievementId);
 }
