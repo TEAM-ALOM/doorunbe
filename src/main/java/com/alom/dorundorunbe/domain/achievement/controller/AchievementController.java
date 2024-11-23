967bc7dbb36634853bd27411e7dec2ea5bd4f11d
KyunghoYeom <hoyeom1269@naver.com>
2024-11-23 14:30:16 +0900

    업적 수정 메서드(updateAchievement) 구현

--- src/main/java/com/alom/dorundorunbe/domain/achievement/controller/AchievementController.java
+++ src/main/java/com/alom/dorundorunbe/domain/achievement/controller/AchievementController.java
@@ -3,13 +3,12 @@ package com.alom.dorundorunbe.domain.achievement.controller;
 import com.alom.dorundorunbe.domain.achievement.domain.Achievement;
 import com.alom.dorundorunbe.domain.achievement.dto.create.CreateAchievementRequestDto;
 import com.alom.dorundorunbe.domain.achievement.dto.create.CreateAchievementResponseDto;
+import com.alom.dorundorunbe.domain.achievement.dto.update.UpdateAchievementRequestDto;
+import com.alom.dorundorunbe.domain.achievement.dto.update.UpdateAchievementResponseDto;
 import com.alom.dorundorunbe.domain.achievement.service.AchievementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +24,17 @@ public class AchievementController {
         Long id = achievementService.createAchievement(requestDto);
         Achievement achievement = achievementService.findOneAchievement(id);
         return ResponseEntity.ok(CreateAchievementResponseDto.of(achievement));
+    }
+
+    /**
+     * 업적 수정
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<UpdateAchievementResponseDto> updateAchievement(
+            @PathVariable Long id,
+            @RequestBody UpdateAchievementRequestDto requestDto) {
+        achievementService.updateAchievement(id, requestDto);
+        Achievement updatedAchievement = achievementService.findOneAchievement(id);
+        return ResponseEntity.ok(UpdateAchievementResponseDto.of(updatedAchievement));
     }
 }
