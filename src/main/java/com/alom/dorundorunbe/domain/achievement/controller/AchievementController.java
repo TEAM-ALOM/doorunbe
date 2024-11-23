da18533fafc738a7d1f28753b7b153e73e292fef
KyunghoYeom <hoyeom1269@naver.com>
2024-11-23 14:31:31 +0900

    사용자 업적 전체 조회 메서드(fetchUserAchievements) 구현

--- src/main/java/com/alom/dorundorunbe/domain/achievement/controller/AchievementController.java
+++ src/main/java/com/alom/dorundorunbe/domain/achievement/controller/AchievementController.java
@@ -3,10 +3,14 @@ package com.alom.dorundorunbe.domain.achievement.controller;
 import com.alom.dorundorunbe.domain.achievement.domain.Achievement;
 import com.alom.dorundorunbe.domain.achievement.dto.create.CreateAchievementRequestDto;
 import com.alom.dorundorunbe.domain.achievement.dto.create.CreateAchievementResponseDto;
+import com.alom.dorundorunbe.domain.achievement.dto.query.AchievementDto;
 import com.alom.dorundorunbe.domain.achievement.dto.update.UpdateAchievementRequestDto;
 import com.alom.dorundorunbe.domain.achievement.dto.update.UpdateAchievementResponseDto;
 import com.alom.dorundorunbe.domain.achievement.service.AchievementService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,5 +40,13 @@ public class AchievementController {
         achievementService.updateAchievement(id, requestDto);
         Achievement updatedAchievement = achievementService.findOneAchievement(id);
         return ResponseEntity.ok(UpdateAchievementResponseDto.of(updatedAchievement));
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<Slice<AchievementDto>> fetchUserAchievements(
+            @PathVariable Long userId,
+            @PageableDefault(size = 10) Pageable pageable) {
+        Slice<AchievementDto> achievementDtosSlice = achievementService.findUserAchievement(userId, pageable);
+        return ResponseEntity.ok(achievementDtosSlice);
     }
 }
