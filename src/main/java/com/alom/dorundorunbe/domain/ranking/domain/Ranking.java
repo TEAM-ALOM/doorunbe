81cac6f18713ea25fc40798f8a3034278df42783
KyunghoYeom <hoyeom1269@naver.com>
2024-11-28 22:16:54 +0900

    Ranking 변경 필드 제거

--- src/main/java/com/alom/dorundorunbe/domain/ranking/domain/Ranking.java
+++ src/main/java/com/alom/dorundorunbe/domain/ranking/domain/Ranking.java
@@ -23,29 +23,6 @@ public class Ranking extends BaseEntity {
     @Column(name = "rank_id")
     private Long id;
 
-    @OneToOne(fetch = LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
 
-    @Column(name = "rank_level", nullable = true)
-    @Enumerated(EnumType.STRING)
-    private Tier tier; //user에도 rank핗드가 있어야 될 것 같습니다 user.getRank로..일단 true로 테스트
-    //이름 변경->티어
-
-    private long time;
-
-    private long distance;
-
-    private int cadence;
-
-    private int grade;
-
-    public void updateRanking(Tier tier, long distance, long time, int cadence, int grade){
-        this.tier = tier;
-        this.distance = distance;
-        this.time = time;
-        this.cadence = cadence;
-        this.grade = grade;
-    }
 
 }
