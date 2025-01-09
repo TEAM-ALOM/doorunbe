17b751d66fc54c58fe0dc1e9a5371b4af916c5bd
DaeunSon <sn15dueo@naver.com>
2025-01-09 21:59:22 +0900

    add PasswordEncoder

--- src/main/java/com/alom/dorundorunbe/global/config/SecurityConfig.java
+++ src/main/java/com/alom/dorundorunbe/global/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -52,5 +54,10 @@ public class SecurityConfig {
     http.cors(AbstractHttpConfigurer::disable); // 테스트를 위해 CORS 비활성화
 
     return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
