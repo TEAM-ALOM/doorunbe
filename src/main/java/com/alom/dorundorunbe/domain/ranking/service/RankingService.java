6b9cf6914c29063402834fe0b6f474e494f683de
KyunghoYeom <hoyeom1260@naver.com>
2025-01-30 22:30:45 +0900

    feat: 특정 랭킹 방 조회

--- src/main/java/com/alom/dorundorunbe/domain/ranking/service/RankingService.java
+++ src/main/java/com/alom/dorundorunbe/domain/ranking/service/RankingService.java
@@ -7,12 +7,15 @@ import com.alom.dorundorunbe.domain.ranking.repository.RankingRepository;
 import com.alom.dorundorunbe.domain.ranking.repository.UserRankingRepository;
 import com.alom.dorundorunbe.domain.runningrecord.repository.RunningRecordRepository;
 import com.alom.dorundorunbe.domain.user.repository.UserRepository;
+import com.alom.dorundorunbe.global.exception.BusinessException;
+import com.alom.dorundorunbe.global.exception.ErrorCode;
 import com.alom.dorundorunbe.global.util.point.service.PointService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +33,15 @@ public class RankingService {
     public List<RankingResponseDto> findAllRankings() {
         List<Ranking> rankings = rankingRepository.findAll();
         return rankings.stream().map(RankingResponseDto::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public RankingResponseDto findRanking(Long rankingId){
+        Optional<Ranking> ranking = rankingRepository.findById(rankingId);
+        return ranking.map(RankingResponseDto::new)
+                .orElseThrow(() -> new BusinessException(ErrorCode.RANKING_NOT_FOUND));
+
+
     }
 }
 
